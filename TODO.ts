dbba029239f5762592cc7ac3a11501e48b6442df
Safalgautam3636 <seeram3636@gmail.com>
2023-11-15 13:59:21 -0500

    update TODO APis

---
+++ TODO.ts
@@ -0,0 +1,28 @@
+// TODO: Authentication
+// - [ ] POST /api/auth/register - User registration.
+// - [ ] POST /api/auth/login - User login.
+// - [ ] POST /api/auth/logout - User logout.
+// - [ ] GET /api/auth/me - Get the current user's profile.
+
+// TODO: User Management
+// - [ ] GET /api/users - Get all users (Admin access only).
+// - [ ] GET /api/users/:userId - Get user by ID (Admin access only).
+// - [ ] PUT /api/users/:userId - Update user by ID (Admin access only).
+// - [ ] DELETE /api/users/:userId - Delete user by ID (Admin access only).
+
+// TODO: Product Management
+// - [ ] GET /api/products - Get all products.
+// - [ ] GET /api/products/:productId - Get product by ID.
+// - [ ] POST /api/products - Create a new product (Admin access only).
+// - [ ] PUT /api/products/:productId - Update product by ID (Admin access only).
+// - [ ] DELETE /api/products/:productId - Delete product by ID (Admin access only).
+
+// TODO: Order Management
+// - [ ] GET /api/orders - Get all orders for the authenticated user.
+// - [ ] GET /api/orders/:orderId - Get order by ID.
+// - [ ] POST /api/orders - Create a new order.
+// - [ ] PUT /api/orders/:orderId - Update order status (Admin access only).
+// - [ ] DELETE /api/orders/:orderId - Delete order by ID (Admin access only).
+
+// TODO: Admin Access Control
+// - [ ] Implement middleware for admin access control to restrict certain routes to users with admin privileges.
