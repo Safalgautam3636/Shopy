5cefc21c2ef2e814a0a5d2eb519abac756579500
Safalgautam3636 <seeram3636@gmail.com>
2023-11-28 02:12:24 -0500

    Update product inventory function

--- server/main/orders/createOrder.ts
+++ server/main/orders/createOrder.ts
@@ -1,13 +1,24 @@
-
+import { ObjectId } from 'mongodb';
 import { Order } from "../models/interfaces/Order";
 import validateOrderSchema from "../models/joi/order";
 import OrderModel from "../models/schemas/Order";
+import ProductModel, { ProductDocument } from "../models/schemas/Product";
 import { URequest, UResponse } from "../types";
 
 const createOrder = async (req: URequest, res: UResponse) => {
     try {
         let order = req.body;
-        const orderObject:Order={...order,userId:req.user}
+
+        const orderObject: Order = { ...order, userId: req.user }
+        orderObject.items.forEach(async(item) => {
+            const product: ProductDocument|null= await ProductModel.findOne({ _id: item.productId });
+            if (product?.stockQuantity !== undefined) {
+                if (item.quantity <= product?.stockQuantity) {
+                    product.stockQuantity -= item.quantity;
+                    await product.save();
+                }
+            }
+        })
         const { error, value } = validateOrderSchema.validate(orderObject);
         if (!error) {
             const order = new OrderModel(value);
