03bdadb9a8ab795a39c14497567e5b6698fcefc6
Safalgautam3636 <seeram3636@gmail.com>
2023-12-02 12:58:40 -0500

    fix product all routes

--- server/main/routes/productRoutes.ts
+++ server/main/routes/productRoutes.ts
@@ -3,20 +3,22 @@ import express from "express";
 import deleteProdut from "../products/deleteProduct";
 import { getAllProducts, getProductById } from "../products/getProduct";
 import updateProduct from "../products/updateProduct";
-import {getProduct,getProducts,deleteProduct,deleteProducts } from "../admin/product"
+import { getProduct, getProducts, deleteProduct, deleteProducts } from "../admin/product"
+import authenticateUser from "../auth/authHelpers/auth";
+import authenticateAdmin from "../auth/authHelpers/admin";
+
 const productRoute = express.Router();
 
-productRoute.post("/create-product", createProduct);
-productRoute.delete("/delete-product/:id", deleteProdut);
-productRoute.get("/get-products", getAllProducts);
-productRoute.get("/get-product/:id", getProductById);
-productRoute.put("/update-product/:id", updateProduct);
+
+//normal user
+productRoute.get("/product/products", getAllProducts);
+productRoute.get("/product/product/:id", getProductById);
 
 // admin routes
-productRoute.get("/admin/product/:id/", getProduct);
-productRoute.get("/admin/products", getProducts);
-productRoute.get("/admin/delete-product/:id/", deleteProduct);
-productRoute.get("/admin/delete-products/", deleteProducts);
+productRoute.post("/admin/product/create-product/", [authenticateUser, authenticateAdmin], createProduct);
+productRoute.put("/admin/product/update-product/:id/", [authenticateUser, authenticateAdmin], updateProduct);
+productRoute.delete("/admin/product/delete-product/:id/", [authenticateUser, authenticateAdmin], deleteProdut);
+productRoute.delete("/admin/product/delete-products/", [authenticateUser, authenticateAdmin], deleteProducts);
 
 
 
