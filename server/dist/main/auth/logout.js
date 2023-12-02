21b8f6d2ec2c77b570a9ee9bc75f0bc7f4938af9
Safalgautam3636 <seeram3636@gmail.com>
2023-12-02 02:30:08 -0500

    remove dist

--- server/dist/main/auth/logout.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const logout = (req, res, next) => {
-    const { username, password } = req.body.user;
-};
-exports.default = logout;
