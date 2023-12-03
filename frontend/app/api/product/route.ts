bfb86f46bb7794f306e2a88e54942e2e66214453
Derek <109226053+derekjtong@users.noreply.github.com>
2023-12-03 01:55:40 -0500

    Add frontend API calls

---
+++ frontend/app/api/product/route.ts
@@ -0,0 +1,54 @@
+import axios, { AxiosResponse } from "axios";
+import { Product } from "@/types/Product"; // Importing the Product interface
+
+const BASE_URL = process.env.BACKEND_URL || "http://localhost:4500/api";
+
+interface ProductResponse {
+  data: Product;
+}
+
+interface ProductsResponse {
+  allProducts: Product[];
+}
+
+// Standard user functions
+export const getAllProducts = async (): Promise<AxiosResponse<ProductsResponse>> => {
+  return axios.get<ProductsResponse>(`${BASE_URL}/product/products`);
+};
+
+export const getProductById = async (id: string): Promise<AxiosResponse<ProductResponse>> => {
+  return axios.get<ProductResponse>(`${BASE_URL}/product/product/${id}`);
+};
+
+// Admin functions
+export const createProduct = async (productData: Product, authToken: string): Promise<AxiosResponse<ProductResponse>> => {
+  return axios.post<ProductResponse>(`${BASE_URL}/admin/product/create-product`, productData, {
+    headers: {
+      Authorization: `Bearer ${authToken}`,
+    },
+  });
+};
+
+export const updateProduct = async (id: string, productData: Product, authToken: string): Promise<AxiosResponse<ProductResponse>> => {
+  return axios.put<ProductResponse>(`${BASE_URL}/admin/product/update-product/${id}`, productData, {
+    headers: {
+      Authorization: `Bearer ${authToken}`,
+    },
+  });
+};
+
+export const deleteProduct = async (id: string, authToken: string): Promise<AxiosResponse> => {
+  return axios.delete(`${BASE_URL}/admin/product/delete-product/${id}`, {
+    headers: {
+      Authorization: `Bearer ${authToken}`,
+    },
+  });
+};
+
+export const deleteProducts = async (authToken: string): Promise<AxiosResponse> => {
+  return axios.delete(`${BASE_URL}/admin/product/delete-products`, {
+    headers: {
+      Authorization: `Bearer ${authToken}`,
+    },
+  });
+};
