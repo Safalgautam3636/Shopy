6af454e2f254100e1ea8d88ba9f0d128cad4a172
Derek <109226053+derekjtong@users.noreply.github.com>
2023-12-03 03:01:50 -0500

    Add product item link to individual page

--- frontend/app/(homepage)/_components/ProductItem.tsx
+++ frontend/app/(homepage)/_components/ProductItem.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import { Product } from "@/types/Product";
+import Link from "next/link";
 
 interface ProductItemProps {
   product: Product;
@@ -7,22 +8,24 @@ interface ProductItemProps {
 
 const ProductItem: React.FC<ProductItemProps> = ({ product }) => {
   return (
-    <div className="max-w-sm overflow-hidden rounded shadow-lg">
-      <div className="relative h-48 w-full">
-        <Image
-          src={product.imgUrl}
-          alt={product.name}
-          fill={true}
-          style={{ objectFit: "cover" }}
-          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 25vw"
-        />
+    <Link href={`/product/${product._id}`} passHref>
+      <div className="max-w-sm overflow-hidden rounded shadow-lg">
+        <div className="relative h-48 w-full">
+          <Image
+            src={product.imgUrl}
+            alt={product.name}
+            fill={true}
+            style={{ objectFit: "cover" }}
+            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 25vw"
+          />
+        </div>
+        <div className="px-6 py-4">
+          <div className="mb-2 text-xl font-bold">{product.name}</div>
+          <p className="text-base text-gray-700">${product.price}</p>
+          {/* Other details */}
+        </div>
       </div>
-      <div className="px-6 py-4">
-        <div className="mb-2 text-xl font-bold">{product.name}</div>
-        <p className="text-base text-gray-700">${product.price}</p>
-        {/* Other details */}
-      </div>
-    </div>
+    </Link>
   );
 };
 
