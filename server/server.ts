e61c0d71d5b85b7b3894bc4c18e410014e7c3fb6
Derek <109226053+derekjtong@users.noreply.github.com>
2023-12-03 01:52:39 -0500

    Add cors

--- server/server.ts
+++ server/server.ts
@@ -3,28 +3,25 @@ import dotenv from "dotenv";
 import invokeDB from "./main/db/setupDb";
 import userRouter from "./main/routes/userRoutes";
 import productRoute from "./main/routes/productRoutes";
-import bodyParser from "body-parser"
+import bodyParser from "body-parser";
 import orderRouter from "./main/routes/orderRoutes";
+import cors from "cors";
 
-dotenv.config()
+dotenv.config();
 
 const app: Express = express();
 
-
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use("/api/",userRouter);
-app.use('/api/', productRoute);
-app.use("/api/",orderRouter);
-
-
+app.use("/api/", userRouter);
+app.use("/api/", productRoute);
+app.use("/api/", orderRouter);
 
 app.listen(process.env.PORT, () => {
   const dbString = process.env.DB_STRING as string;
   invokeDB(dbString);
-  return console.log(
-    `Express is  at http://localhost:${process.env.PORT}`
-  );
+  return console.log(`Express is  at http://localhost:${process.env.PORT}`);
 });
