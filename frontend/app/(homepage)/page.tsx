b718e750d1eb2f41dcf1b07331c53716cfd478dc
Derek <109226053+derekjtong@users.noreply.github.com>
2023-12-03 02:59:55 -0500

    Remove old components

--- frontend/app/(homepage)/page.tsx
+++ frontend/app/(homepage)/page.tsx
@@ -5,9 +5,7 @@ export default function Home() {
   return (
     <div className="flex min-h-full flex-col">
       <div className="flex flex-1 flex-col items-center justify-center gap-y-8 px-6 pb-10 text-center md:justify-start">
-        <Search />
         <ItemGrid />
-        {/* <Heroes /> */}
       </div>
       {/* <Footer /> */}
     </div>
