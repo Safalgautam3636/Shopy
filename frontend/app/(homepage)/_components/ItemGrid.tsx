ca056d0d171ab2eb4e4f184276677cc6364e0c61
Derek <109226053+derekjtong@users.noreply.github.com>
2023-12-03 21:21:48 -0500

    Add sort dropdown

--- frontend/app/(homepage)/_components/ItemGrid.tsx
+++ frontend/app/(homepage)/_components/ItemGrid.tsx
@@ -4,11 +4,14 @@ import { getAllProducts } from "@/app/api/product/route";
 import { Product } from "@/types/Product";
 import ProductItem from "./ProductItem";
 import { Spinner } from "@/components/Spinner";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+import { Button } from "@/components/ui/button";
 
 function ItemGrid() {
   const [products, setProducts] = useState<Product[]>([]);
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState<any>(null);
+  const [sort, setSort] = useState("No Sorting");
 
   useEffect(() => {
     const fetchProducts = async () => {
@@ -25,7 +28,26 @@ function ItemGrid() {
     fetchProducts();
   }, []);
 
-  const cachedProductList = useMemo(() => products, [products]);
+  const sortedProducts = useMemo(() => {
+    let sorted = [...products];
+    switch (sort) {
+      case "Highest to Lowest":
+        sorted.sort((a, b) => b.price - a.price);
+        break;
+      case "Lowest to Highest":
+        sorted.sort((a, b) => a.price - b.price);
+        break;
+      case "Alphabetical A-Z":
+        sorted.sort((a, b) => a.name.localeCompare(b.name));
+        break;
+      case "Alphabetical Z-A":
+        sorted.sort((a, b) => b.name.localeCompare(a.name));
+        break;
+      default:
+        break;
+    }
+    return sorted;
+  }, [products, sort]);
 
   if (isLoading) {
     return <Spinner />;
@@ -35,14 +57,32 @@ function ItemGrid() {
     return <div>Error: {error.message}</div>;
   }
 
-  if (!cachedProductList || cachedProductList.length === 0) {
+  if (!sortedProducts || sortedProducts.length === 0) {
     return <div>Error: products not found</div>;
   }
 
   return (
     <div className="container mx-auto px-4">
+      <div className="pb-5">
+        Sort By:{" "}
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="outline" size="sm">
+              {sort}
+              <span className="sr-only"></span>
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuItem onClick={() => setSort("No Sorting")}>No Sorting</DropdownMenuItem>
+            <DropdownMenuItem onClick={() => setSort("Highest to Lowest")}>Highest to Lowest</DropdownMenuItem>
+            <DropdownMenuItem onClick={() => setSort("Lowest to Highest")}>Lowest to Highest</DropdownMenuItem>
+            <DropdownMenuItem onClick={() => setSort("Alphabetical A-Z")}>Alphabetical A-Z</DropdownMenuItem>
+            <DropdownMenuItem onClick={() => setSort("Alphabetical Z-A")}>Alphabetical Z-A</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
       <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-        {cachedProductList.map((product) => (
+        {sortedProducts.map((product) => (
           <ProductItem key={product._id} product={product} />
         ))}
       </div>
