5471605cb93468c683eef839a7383fab18e76075
Derek <109226053+derekjtong@users.noreply.github.com>
2023-12-03 20:47:25 -0500

    Adjust schema to match data

--- frontend/types/Product.ts
+++ frontend/types/Product.ts
@@ -2,11 +2,9 @@ export interface Product {
   _id?: string;
   imgUrl: string;
   name: string;
-  description?: string;
   price: number;
   stockQuantity: number;
   category: string;
-  brand?: string;
   ratings: number;
   reviews: number;
 }
