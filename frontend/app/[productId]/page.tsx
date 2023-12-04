771adce16a769d4faa74897131a079ebf61e7e39
Derek <109226053+derekjtong@users.noreply.github.com>
2023-12-03 20:47:33 -0500

    Add product page

--- frontend/app/[productId]/page.tsx
+++ frontend/app/[productId]/page.tsx
@@ -2,14 +2,16 @@
 import { getProductById } from "@/app/api/product/route";
 import { Spinner } from "@/components/Spinner";
 import { Product } from "@/types/Product";
+import Image from "next/image";
 import { useParams } from "next/navigation";
 import { useEffect, useState } from "react";
 
-export default function Home() {
+export default function ProductPage() {
   const [product, setProduct] = useState<Product | null>(null);
   const [isLoading, setIsLoading] = useState(true);
   const params = useParams();
   const productId = params.productId;
+
   useEffect(() => {
     if (productId) {
       getProductById(productId as string)
@@ -23,17 +25,36 @@ export default function Home() {
         });
     }
   }, [productId]);
+
   if (isLoading) {
     return (
-      <div className="flex h-screen items-center justify-center pt-24">
+      <div className="flex h-screen items-center justify-center">
         <Spinner size="lg" />
       </div>
     );
   }
-  if (!product) return <div>Loading...</div>;
+
+  if (!product) {
+    return <div>Error: Product not found</div>;
+  }
+
   return (
-    <div className="pt-24">
-      <h1>{product.name}dsads</h1>
+    <div className="container mt-24">
+      <div className="flex">
+        <div className="mt-16 w-1/2">
+          <Image src={product.imgUrl} alt={product.name} width={500} height={500} />
+        </div>
+        <div className="w-1/2 pl-8">
+          <p className="mb-4 text-lg text-gray-700">{product.category}</p>
+          <h1 className="mb-4 text-3xl font-semibold">{product.name}</h1>
+          <p className="mb-4 text-lg text-gray-700">${product.price}</p>
+          <p className="mb-6 text-gray-600">In stock: {product.stockQuantity}</p>
+          <div className="mb-6 flex items-center">
+            <span className="mr-2 text-gray-700">Rating: {product.ratings}</span>
+          </div>
+          <button className="rounded bg-blue-500 px-4 py-2 text-white transition duration-300 hover:bg-blue-600">Add to Cart</button>
+        </div>
+      </div>
     </div>
   );
 }
