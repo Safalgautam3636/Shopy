4a61f1b99575adeab17b80165e73fc56893427f2
Derek <109226053+derekjtong@users.noreply.github.com>
2023-12-07 03:59:33 -0500

    Add product card add to cart button

--- frontend/app/(homepage)/_components/ProductCard.tsx
+++ frontend/app/(homepage)/_components/ProductCard.tsx
@@ -1,15 +1,36 @@
 import Image from "next/image";
 import { Product } from "@/types/Product";
 import Link from "next/link";
+import { useState } from "react";
+import { FaRegTrashAlt } from "react-icons/fa";
 
 interface ProductItemProps {
   product: Product;
 }
 
 const ProductItem: React.FC<ProductItemProps> = ({ product }) => {
+  const [isAdded, setIsAdded] = useState(false);
+  const [quantity, setQuantity] = useState(0);
+
+  function handleAddToCart() {
+    console.log("Add to cart:", product._id);
+    setQuantity(1);
+    setIsAdded(true);
+  }
+  function handleIncrement() {
+    setQuantity((cur) => cur + 1);
+  }
+  function handleDecrement() {
+    setQuantity((cur) => (cur > 1 ? cur - 1 : 1));
+  }
+  function handleRemove() {
+    setQuantity(0);
+    setIsAdded(false);
+  }
+
   return (
-    <Link href={`/${product._id}`} passHref>
-      <div className="max-w-sm overflow-hidden rounded shadow-lg dark:shadow-slate-700">
+    <div className="max-w-sm overflow-hidden rounded shadow-lg dark:shadow-slate-700">
+      <Link href={`/${product._id}`} passHref>
         <div className="relative h-48 w-full">
           <Image
             src={product.imgUrl}
@@ -24,8 +45,33 @@ const ProductItem: React.FC<ProductItemProps> = ({ product }) => {
           <p className="text-base text-gray-700 dark:text-gray-300">${product.price}</p>
           {/* Other details */}
         </div>
-      </div>
-    </Link>
+      </Link>
+      {!isAdded ? (
+        <button className="w-full rounded-b bg-blue-500 px-6 py-2 text-sm font-bold text-white hover:bg-blue-600" onClick={handleAddToCart}>
+          Add to Cart
+        </button>
+      ) : (
+        <div className="flex items-center justify-between rounded-b bg-blue-500 text-sm font-bold text-white">
+          <div className="flex flex-1 justify-start">
+            {quantity === 1 ? (
+              <button onClick={handleRemove} className="h-full px-6 py-2.5 hover:bg-blue-600">
+                <FaRegTrashAlt />
+              </button>
+            ) : (
+              <button onClick={handleDecrement} className="h-full px-6 py-2 hover:bg-blue-600">
+                -
+              </button>
+            )}
+          </div>
+          <span className="flex-1 text-center">{quantity}</span>
+          <div className="flex flex-1 justify-end">
+            <button onClick={handleIncrement} className="h-full px-6 py-2 hover:bg-blue-600">
+              +
+            </button>
+          </div>
+        </div>
+      )}
+    </div>
   );
 };
 
