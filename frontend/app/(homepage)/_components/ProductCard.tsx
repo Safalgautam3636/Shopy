3db7109fc350ae991c09a80c917639784219b631
Derek <109226053+derekjtong@users.noreply.github.com>
2023-12-07 04:43:48 -0500

    Improve dark mode colors

--- frontend/app/(homepage)/_components/ProductCard.tsx
+++ frontend/app/(homepage)/_components/ProductCard.tsx
@@ -41,31 +41,34 @@ const ProductItem: React.FC<ProductItemProps> = ({ product }) => {
           />
         </div>
         <div className="px-6 py-4">
-          <div className="mb-2 truncate text-lg font-bold">{product.name}</div>
+          <div className="mb-2 truncate text-lg font-bold dark:text-gray-300">{product.name}</div>
           <p className="text-base text-gray-700 dark:text-gray-300">${product.price}</p>
           {/* Other details */}
         </div>
       </Link>
       {!isAdded ? (
-        <button className="w-full rounded-b bg-blue-500 px-6 py-2 text-sm font-bold text-white hover:bg-blue-600" onClick={handleAddToCart}>
+        <button
+          className="w-full rounded-b bg-blue-500 px-6 py-2 text-sm font-bold text-white hover:bg-blue-600 dark:bg-blue-900 dark:text-gray-300 dark:hover:bg-blue-950"
+          onClick={handleAddToCart}
+        >
           Add to Cart
         </button>
       ) : (
-        <div className="flex items-center justify-between rounded-b bg-blue-500 text-sm font-bold text-white">
+        <div className="flex items-center justify-between rounded-b bg-blue-500 text-sm font-bold text-white dark:bg-blue-900">
           <div className="flex flex-1 justify-start">
             {quantity === 1 ? (
-              <button onClick={handleRemove} className="h-full px-6 py-2.5 hover:bg-blue-600">
+              <button onClick={handleRemove} className="h-full px-6 py-2.5 hover:bg-blue-600 dark:bg-blue-900 dark:hover:bg-blue-950">
                 <FaRegTrashAlt />
               </button>
             ) : (
-              <button onClick={handleDecrement} className="h-full px-6 py-2 hover:bg-blue-600">
+              <button onClick={handleDecrement} className="h-full px-6 py-2 hover:bg-blue-600 dark:bg-blue-900 dark:hover:bg-blue-950">
                 -
               </button>
             )}
           </div>
           <span className="flex-1 text-center">{quantity}</span>
           <div className="flex flex-1 justify-end">
-            <button onClick={handleIncrement} className="h-full px-6 py-2 hover:bg-blue-600">
+            <button onClick={handleIncrement} className="h-full px-6 py-2 hover:bg-blue-600 dark:bg-blue-900 dark:hover:bg-blue-950">
               +
             </button>
           </div>
