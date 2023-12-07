1d5130e7e548096243f9bd080c402dce71d8de10
Derek <109226053+derekjtong@users.noreply.github.com>
2023-12-07 02:29:35 -0500

    Rename homepage layout component

--- frontend/app/(homepage)/layout.tsx
+++ frontend/app/(homepage)/layout.tsx
@@ -1,8 +1,7 @@
 import React from "react";
-import Navbar from "../../components/Navbar";
 
-function MarketingLayout({ children }: { children: React.ReactNode }) {
+function HomeLayout({ children }: { children: React.ReactNode }) {
   return <main className="h-full">{children}</main>;
 }
 
-export default MarketingLayout;
+export default HomeLayout;
