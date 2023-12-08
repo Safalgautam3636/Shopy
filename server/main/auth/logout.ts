9e2867fab2abf46dc299a44145c4088e9f07adc5
Derek <109226053+derekjtong@users.noreply.github.com>
2023-12-08 05:57:25 -0500

    Remove logout placeholder

--- server/main/auth/logout.ts
+++ server/main/auth/logout.ts
@@ -4,6 +4,5 @@ import { URequest, UResponse } from "../types";
 const logout = (req: URequest, res: UResponse, next: NextFunction) => {
   // TODO: Pass credentials?
   console.log("logout attempt");
-  const { username, password } = req.body.user;
 };
 export default logout;
