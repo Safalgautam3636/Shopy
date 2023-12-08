b1636729f113b048e18991493faa53d95c03fc8d
Derek <109226053+derekjtong@users.noreply.github.com>
2023-12-08 06:17:25 -0500

    Add stock to product card

--- frontend/app/(homepage)/ProductCard.tsx
+++ frontend/app/(homepage)/ProductCard.tsx
@@ -1,7 +1,8 @@
+"use client";
 import Image from "next/image";
 import { Product } from "@/types/Product";
 import Link from "next/link";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { formatPrice } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 import { CartContext } from "@/components/providers/cart-provider";
@@ -12,6 +13,15 @@ interface ProductItemV2Props {
 
 const ProductItem: React.FC<ProductItemV2Props> = ({ product }) => {
   const { incrementCart } = useContext(CartContext);
+  const [isAdded, setIsAdded] = useState(false);
+
+  const handleAddToCart = () => {
+    incrementCart(product);
+    setIsAdded(true);
+    setTimeout(() => {
+      setIsAdded(false);
+    }, 1000);
+  };
 
   return (
     <div className="rounded-lg bg-white px-10 py-10 shadow-md dark:bg-gray-800 dark:shadow-lg">
@@ -33,11 +43,17 @@ const ProductItem: React.FC<ProductItemV2Props> = ({ product }) => {
           <h1 className="truncate text-lg font-bold uppercase text-gray-900 dark:text-white">{product.name}</h1>
           <p className="mt-2 text-sm text-gray-600 dark:text-gray-300">{product.category}</p>
         </Link>
-        <p className="mt-2 text-gray-900 dark:text-gray-100">{formatPrice(product.price)}</p>
+        <div className="flex justify-between">
+          <p className="mt-2 text-gray-900 dark:text-gray-100">{formatPrice(product.price)}</p>
+          <p className="mt-2 text-gray-900 dark:text-gray-100">
+            {product.stockQuantity === 0 ? "Out of Stock" : "In stock: " + product.stockQuantity}
+          </p>
+        </div>
       </div>
       <div className="mt-6 flex items-center justify-between">
-        <Button className="rounded text-xs font-bold uppercase" onClick={() => incrementCart(product)}>
-          Add to cart
+        <Button className="rounded text-xs font-bold uppercase" onClick={handleAddToCart} disabled={isAdded}>
+          <span className={`${isAdded ? "hidden" : "px-1"}`}>add to cart</span>
+          <span className={`${!isAdded ? "hidden" : "px-6"}`}>added</span>
         </Button>
       </div>
     </div>
