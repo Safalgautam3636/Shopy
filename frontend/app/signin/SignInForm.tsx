83e6901c64fe6453922701a2b95b59bc5dfae69a
Derek <109226053+derekjtong@users.noreply.github.com>
2023-12-07 21:25:28 -0500

    Change to div

---
+++ frontend/app/signin/SignInForm.tsx
@@ -0,0 +1,78 @@
+"use client";
+
+import * as React from "react";
+
+import { cn } from "@/lib/utils";
+import { Spinner } from "@/components/Spinner";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import axios from "axios";
+
+interface UserAuthFormProps extends React.HTMLAttributes<HTMLDivElement> {}
+
+export function SignInForm({ className, ...props }: UserAuthFormProps) {
+  const [isLoading, setIsLoading] = React.useState<boolean>(false);
+  const [username, setUsername] = React.useState<string>("");
+  const [password, setPassword] = React.useState<string>("");
+  const [error, setError] = React.useState<string>("");
+
+  async function onSubmit(event: React.SyntheticEvent) {
+    event.preventDefault();
+    setIsLoading(true);
+    try {
+      const response = await axios.post(`${process.env.NEXT_PUBLIC_BACKEND_URL}/user/login`, { username, password });
+      console.log(response);
+      if (response.data.message === "User does not exist") {
+        setError("User does not exist");
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  return (
+    <div className={cn("grid gap-6", className)} {...props}>
+      <form onSubmit={onSubmit}>
+        <div className="grid gap-2">
+          <div className="grid gap-1">
+            Username
+            <Input
+              id="username"
+              type="text"
+              autoCapitalize="none"
+              autoComplete="username"
+              autoCorrect="off"
+              disabled={isLoading}
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              required={true}
+            />
+            Password
+            <Input
+              id="password"
+              type="password"
+              autoCapitalize="none"
+              autoComplete="email"
+              autoCorrect="off"
+              disabled={isLoading}
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required={true}
+            />
+          </div>
+          <Button disabled={isLoading}>
+            {isLoading && (
+              <div className="mr-2 h-4 w-4 animate-spin">
+                <Spinner />
+              </div>
+            )}
+            Sign In
+          </Button>
+          <div className="text-sm text-red-600">{error}</div>
+        </div>
+      </form>
+    </div>
+  );
+}
