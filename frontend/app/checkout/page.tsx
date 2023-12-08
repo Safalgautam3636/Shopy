3647ce9e2cd97d0e183894487e08bae591f91b09
Derek <109226053+derekjtong@users.noreply.github.com>
2023-12-08 16:22:43 -0500

    Clear cart on purchase

--- frontend/app/checkout/page.tsx
+++ frontend/app/checkout/page.tsx
@@ -1,10 +1,14 @@
+"use client";
+import { CartContext } from "@/components/providers/cart-provider";
 import { Button } from "@/components/ui/button";
+import { useContext } from "react";
 
 function CheckoutPage() {
+  const { clearCart } = useContext(CartContext);
   return (
     <div className="flex justify-center">
       <div>
-        <Button>TEMPORARY - Confirm checkout and remove from database</Button>
+        <Button onClick={() => clearCart()}>TEMPORARY - Confirm checkout and remove from database</Button>
       </div>
     </div>
   );
