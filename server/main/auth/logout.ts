ed357a731d4bf6da701c9e19ddf275541712a6ab
Derek <109226053+derekjtong@users.noreply.github.com>
2023-12-08 04:22:49 -0500

    Remove unused hook

--- server/main/auth/logout.ts
+++ server/main/auth/logout.ts
@@ -2,8 +2,8 @@ import { NextFunction, Request, Response } from "express";
 import User from "../models/interfaces/User";
 import { URequest, UResponse } from "../types";
 const logout = (req: URequest, res: UResponse, next: NextFunction) => {
+  // TODO: Pass credentials?
   console.log("logout attempt");
   const { username, password } = req.body.user;
 };
 export default logout;
- 
