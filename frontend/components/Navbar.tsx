08afe5c07743f977c13970af1273e23ee8e016c9
Derek <109226053+derekjtong@users.noreply.github.com>
2023-12-08 03:00:34 -0500

    Adjust settings icon dark mode

--- frontend/components/Navbar.tsx
+++ frontend/components/Navbar.tsx
@@ -71,8 +71,8 @@ function Navbar() {
           </>
         )}
         <Link href="/dashboard">
-          <Button variant="outline" size="icon">
-            <Settings className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+          <Button variant="ghost" size="icon">
+            <Settings className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 " />
           </Button>
         </Link>
       </div>
