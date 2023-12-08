88aaa4d1b2d90081fc86e4b418ce7d2c89e57b5d
Derek <109226053+derekjtong@users.noreply.github.com>
2023-12-07 23:42:37 -0500

    Fix token split

--- server/main/auth/authHelpers/auth.ts
+++ server/main/auth/authHelpers/auth.ts
@@ -1,27 +1,35 @@
-import Jwt from 'jsonwebtoken';
+import Jwt from "jsonwebtoken";
 import { Request, Response, NextFunction } from "express";
-import { verifyToken } from './jwtHelpers';
+import { verifyToken } from "./jwtHelpers";
 import { URequest, UResponse } from "../../types";
 
-type UserResponse = { _id: string, isAdmin: boolean };
+type UserResponse = { _id: string; isAdmin: boolean };
 type Decoded = {
-    user: UserResponse,
-    iat: number,
-    exp: number
-}
-const authenticateUser = (req: URequest, res: UResponse, next: NextFunction) => {
-    const token = req.header('Auth');
-    if (!token) {
-        return res.json({
-            message: 'No token provided',
-        })
+  user: UserResponse;
+  iat: number;
+  exp: number;
+};
+const authenticateUser = (
+  req: URequest,
+  res: UResponse,
+  next: NextFunction
+) => {
+  console.log("Token received: " + req.header("authorization"));
+  const token = req.headers.authorization?.split(" ")[1];
+  if (!token) {
+    return res.json({
+      message: "No token provided",
+    });
+  } else {
+    try {
+      const decoded: Decoded = verifyToken(token) as Decoded;
+      const user: UserResponse = decoded.user;
+      req.user = user._id;
+      next();
+    } catch (err) {
+      console.log(err);
     }
-    else {
-        const decoded: Decoded = verifyToken(token) as Decoded;
-        const user: UserResponse= decoded.user;
-        req.user = user._id;
-        next();
-    }
-}
+  }
+};
 
 export default authenticateUser;
