9f88350f4412a9c743e93c748777336eebdd4eb3
Derek <109226053+derekjtong@users.noreply.github.com>
2023-12-08 15:55:20 -0500

    Add placeholder checkout

---
+++ frontend/app/checkout/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+
+function CheckoutPage() {
+  return (
+    <div className="flex justify-center">
+      <div>
+        <Button>TEMPORARY - Confirm checkout and remove from database</Button>
+      </div>
+    </div>
+  );
+}
+
+export default CheckoutPage;
