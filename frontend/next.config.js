23edea067cbe1711ef972b9d4803ff3aa24e6338
Derek <109226053+derekjtong@users.noreply.github.com>
2023-12-09 03:46:43 -0500

    Update deprecated nextjs image.domains

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["m.media-amazon.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "m.media-amazon.com",
+        pathname: "**",
+      },
+    ],
   },
 };
 
