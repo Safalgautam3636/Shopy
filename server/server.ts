bb674846f79db29e2cf60fe630f5cc0334f8fb8d
Derek <109226053+derekjtong@users.noreply.github.com>
2023-12-14 16:46:18 -0500

    Add middleware request logging

--- server/server.ts
+++ server/server.ts
@@ -16,6 +16,13 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use((req, res, next) => {
+  console.log("Incoming Request:", req.method, req.path);
+  if (Object.keys(req.body).length !== 0) {
+    console.log("    Body:", req.body);
+  }
+  next();
+});
 
 app.use("/api/", userRouter);
 app.use("/api/", productRoute);
